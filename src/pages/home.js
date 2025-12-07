9260f845119350034cdd61e5b349a12123d38eeb
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2025-12-08 01:07:52 +0200

    type the rights and change the favIcon and tab name

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,12 +4,15 @@ import ProductCart from '../components/productCart'
 const Home = () => {
   return (
     <div>
+      <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-5'>List Products</h1>
-        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
-            {products.map((product, key) => 
-                <ProductCart key={key} data={product}/>
-            )}
-        </div>
+        <h2>This Add to Cart created by <a href='https://abdelrahman-profile.netlify.app/' target='_blank' rel="noreferrer" className='text-blue-500'>AbdelRahman</a></h2>
+      </div>
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
+        {products.map((product, key) =>
+          <ProductCart key={key} data={product} />
+        )}
+      </div>
     </div>
   )
 }
